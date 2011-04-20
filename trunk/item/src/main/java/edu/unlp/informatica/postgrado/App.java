eeb264f36f44f8fc4d5cd6a49a933552aa4353ac
dariovmartine@gmail.com <dariovmartine@gmail.com@618a4134-322e-dd68-9d12-e2e6020e4400>
2011-04-20 00:36:43 +0000

    Creé el modulo 'item' y se creó un test de prueba.

---
+++ trunk/item/src/main/java/edu/unlp/informatica/postgrado/App.java
@@ -0,0 +1,13 @@
+package edu.unlp.informatica.postgrado;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
