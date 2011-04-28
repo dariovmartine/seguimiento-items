67c8d184544d5501f07fdceb7f060fe00598d80f
dariovmartine <dariovmartine@618a4134-322e-dd68-9d12-e2e6020e4400>
2011-04-28 15:43:29 +0000

    Ahora funciona la validacion personal y ademas volé el dto y lo pasé al backend.

---
+++ trunk/seguimiento/item/src/test/java/edu/unlp/informatica/postgrado/ItemBeanValidationTest.java
@@ -0,0 +1,39 @@
+package edu.unlp.informatica.postgrado;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.unlp.informatica.postgrado.seguimiento.item.model.Item;
+
+public class ItemBeanValidationTest {
+
+	private static Validator validator;
+
+	@BeforeClass
+	public static void setUp() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+	@Test
+	public void test() {
+		Item bean = new Item();
+
+		Set<ConstraintViolation<Item>> constraintViolations = validator
+				.validate(bean);
+		assertEquals(0, constraintViolations.size());
+		if (constraintViolations.size() >0) {
+			System.out.println(constraintViolations.iterator().next().getMessage());
+		}
+	}
+}
+
