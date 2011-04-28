a33e8d2a5c38f24f2af58047fb7704da68074215
dariovmartine@gmail.com <dariovmartine@gmail.com@618a4134-322e-dd68-9d12-e2e6020e4400>
2011-04-28 23:26:07 +0000

    Ahora se conecta y crea la base de datos.

--- trunk/seguimiento/item/src/main/java/edu/unlp/informatica/postgrado/seguimiento/item/model/Item.java
+++ trunk/seguimiento/item/src/main/java/edu/unlp/informatica/postgrado/seguimiento/item/model/Item.java
@@ -1,20 +1,45 @@
 package edu.unlp.informatica.postgrado.seguimiento.item.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import edu.unlp.informatica.postgrado.seguimiento.item.validators.ValidUserName;
 
 // @ManagedBean    Lo mandamos al faces-config.xml
 // @RequestScoped   Lo mandamos al faces-config.xml
-@Entity(name= "ITEM" )
+@Entity
+@Table(name = "ITEM")
 public class Item {
 
+	@Id 
+	@Column(name = "ID", unique = true, nullable = false, insertable = true, updatable = true)
+	@GeneratedValue(generator="EXAMPLE_ID_GEN", strategy=GenerationType.SEQUENCE)
+	@SequenceGenerator(name="EXAMPLE_ID_GEN", sequenceName="SEQ_PERSON_ID")
+	Long id;
+	
 	@ValidUserName
 	@Size(min = 3, max = 12, message = "Must be between 3 and 12 chars")
+	@Column(name = "NAME")
 	private String name;
 
+	@NotNull
+	@Column(name = "STATE")
 	private String state;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 	
 	public String getName() {
 		return name;
