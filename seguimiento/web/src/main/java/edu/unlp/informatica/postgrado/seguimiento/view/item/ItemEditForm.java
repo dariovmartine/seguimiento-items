acf5c724ee183780a37471c7b60ffb3b9c5c4e6f
dariovmartine <dariovmartine@618a4134-322e-dd68-9d12-e2e6020e4400>
2011-10-11 17:23:14 +0000

    ahora graba el estado y usa el mapper form<->item

--- seguimiento/web/src/main/java/edu/unlp/informatica/postgrado/seguimiento/view/item/ItemEditForm.java
+++ seguimiento/web/src/main/java/edu/unlp/informatica/postgrado/seguimiento/view/item/ItemEditForm.java
@@ -1,7 +1,6 @@
 package edu.unlp.informatica.postgrado.seguimiento.view.item;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.FormComponent;
@@ -10,7 +9,10 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.util.WildcardListModel;
 
-public class ItemEditForm extends Form<edu.unlp.informatica.postgrado.seguimiento.item.model.Item> {
+import edu.unlp.informatica.postgrado.seguimiento.item.model.Item;
+import edu.unlp.informatica.postgrado.seguimiento.view.DataSourceLocator;
+
+public class ItemEditForm extends Form<Item> {
 
 	private static final long serialVersionUID = 7499289901011022854L;	
 
@@ -18,91 +20,19 @@ public class ItemEditForm extends Form<edu.unlp.informatica.postgrado.seguimient
 	
 	private ListMultipleChoice<String> choice;
 	
-	private List<String> estados;
-	
-	private String name;	
-	
-	private Long itemId;
-	
-	public Long getItemId() {
-		return itemId;
-	}
-
-	public void setItemId(Long id) {
-		this.itemId = itemId;
-	}
-
 	public ItemEditForm() {
 		
-		super("inputForm" );// , new CompoundPropertyModel<FormInputModel>(new FormInputModel()));
+		super("inputForm");
 
-		textField = new TextField<String>("firstname").setRequired(true).setLabel(
+		textField = new TextField<String>("name").setRequired(true).setLabel(
 				new Model<String>("String"));
-		textField.setModel(new Model<String>());
+		//textField.setModel(new Model<String>());
 		add(textField);
 		
-		add(choice = new ListMultipleChoice<String>("estadoSelection"));
-		choice.setDefaultModel(new WildcardListModel<Collection<String>>() );
-		
-		
+		add(choice = new ListMultipleChoice<String>("state"));
+		choice.setDefaultModel(new WildcardListModel<Collection<String>>());
+		choice.setChoices(DataSourceLocator.getInstance().getEstadoService().getNames());
 	}
-
-	/**
-	 * @return the textField
-	 */
-	public FormComponent<String> getTextField() {
-		return textField;
-	}
-
-	/**
-	 * @param textField the textField to set
-	 */
-	public void setTextField(FormComponent<String> textField) {
-		this.textField = textField;
-	}
-
 	
-
-	/**
-	 * @return the estados
-	 */
-	public List<String> getEstados() {
-		return estados;
-	}
-
-	/**
-	 * @param estados the estados to set
-	 */
-	public void setEstados(List<String> estados) {
-		this.estados = estados;
-	}
-
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-		this.textField.getModel().setObject(name);
-	}
-
-	/**
-	 * @return the choice
-	 */
-	public ListMultipleChoice<String> getChoice() {
-		return choice;
-	}
-
-	/**
-	 * @param choice the choice to set
-	 */
-	public void setChoice(ListMultipleChoice<String> choice) {
-		this.choice = choice;
-	}
+	
 }
