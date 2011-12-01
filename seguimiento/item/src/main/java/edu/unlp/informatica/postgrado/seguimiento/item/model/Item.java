1d90def7a34fa4901400a968870bf933585467b0
dariovmartine@gmail.com <dariovmartine@gmail.com@618a4134-322e-dd68-9d12-e2e6020e4400>
2011-12-01 19:24:06 +0000

    Ahora anda el listado de items

--- seguimiento/item/src/main/java/edu/unlp/informatica/postgrado/seguimiento/item/model/Item.java
+++ seguimiento/item/src/main/java/edu/unlp/informatica/postgrado/seguimiento/item/model/Item.java
@@ -153,8 +153,6 @@ public class Item implements Serializable {
 		result = prime * result
 				+ ((prioridad == null) ? 0 : prioridad.hashCode());
 		result = prime * result
-				+ ((proyecto == null) ? 0 : proyecto.hashCode());
-		result = prime * result
 				+ ((responsable == null) ? 0 : responsable.hashCode());
 		result = prime * result
 				+ ((tipoItem == null) ? 0 : tipoItem.hashCode());
@@ -185,11 +183,6 @@ public class Item implements Serializable {
 			if (other.prioridad != null)
 				return false;
 		} else if (!prioridad.equals(other.prioridad))
-			return false;
-		if (proyecto == null) {
-			if (other.proyecto != null)
-				return false;
-		} else if (!proyecto.equals(other.proyecto))
 			return false;
 		if (responsable == null) {
 			if (other.responsable != null)
