247c8205b957c95c7360134279ff11594b9efa9a
maria.lujan.salvadori@gmail.com <maria.lujan.salvadori@gmail.com@618a4134-322e-dd68-9d12-e2e6020e4400>
2011-12-22 20:55:32 +0000

    primer commit de Lu, como festejamos?

---
+++ trunk/seguimiento/item/src/main/java/edu/unlp/informatica/postgrado/seguimiento/item/model/HistoriaItem.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package edu.unlp.informatica.postgrado.seguimiento.item.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+
+
+/**
+ * @author lu
+ *
+ */
+@Entity
+@Table(name = "HISTORIA_ITEM")
+	
+public class HistoriaItem {
+
+	
+	@Id 
+	@Column(name = "ID")
+	@GeneratedValue(generator="HIST_ITEM_ID_GEN", strategy=GenerationType.SEQUENCE)
+	@SequenceGenerator(name="HIST_ITEM_ID_GEN", sequenceName="SEQ_HIST_ITEM_ID")
+	Long id;
+	
+	@NotNull
+	@ManyToOne(cascade=CascadeType.DETACH)
+	@JoinColumn(name = "ID_PERSONA")
+	private Persona responsable;
+	
+	@NotNull
+	@ManyToOne(cascade=CascadeType.DETACH)
+	@JoinColumn(name = "ID_ITEM")
+	private Item item;
+	
+	@NotNull
+	@ManyToOne(cascade=CascadeType.DETACH)
+	@JoinColumn(name = "ID_ESTADO")
+	private Persona estado;
+	
+	@NotNull
+	@Column(name = "F_INICIO")
+	private Timestamp fechaInicio;
+	
+	@NotNull
+	@Column(name = "F_FIN")
+	private Timestamp fechaFin;
+}
