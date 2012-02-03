c352f7ad80db55ff9a14833e8d8c880cf2d7079c
dariovmartine <dariovmartine@618a4134-322e-dd68-9d12-e2e6020e4400>
2012-02-03 19:50:08 +0000

    corregi el header

--- trunk/seguimiento/item/src/main/java/edu/unlp/informatica/postgrado/seguimiento/item/model/security/Rol.java
+++ trunk/seguimiento/item/src/main/java/edu/unlp/informatica/postgrado/seguimiento/item/model/security/Rol.java
@@ -3,18 +3,7 @@ package edu.unlp.informatica.postgrado.seguimiento.item.model.security;
 
 public enum Rol {
 
-	ROLE_SUPERVISOR("ROLE_SUPERVISOR"), 
-	ROLE_USER("ROLE_USER"), 
-	ROLE_TELLER("ROLE_TELLER");
-	
-	String value;
-	
-	Rol(String value) {
-		this.value= value;
-	}
-	
-	@Override 
-	public String toString() {
-		return value;
-	}
+	ROLE_SUPERVISOR, 
+	ROLE_USER, 
+	ROLE_DEVELOPER;
 }
