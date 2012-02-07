37033c0f5105e4c55e12de024398837be1840e92
dariovmartine <dariovmartine@618a4134-322e-dd68-9d12-e2e6020e4400>
2012-02-07 19:15:57 +0000

    queda mas lindo asi

--- trunk/seguimiento/web/src/main/java/edu/unlp/informatica/postgrado/seguimiento/view/configuracionestado/ConfiguracionEstadoEditForm.java
+++ trunk/seguimiento/web/src/main/java/edu/unlp/informatica/postgrado/seguimiento/view/configuracionestado/ConfiguracionEstadoEditForm.java
@@ -1,8 +1,14 @@
 package edu.unlp.informatica.postgrado.seguimiento.view.configuracionestado;
 
-import org.apache.wicket.markup.html.form.ListChoice;
-import org.apache.wicket.markup.html.form.ListMultipleChoice;
+import org.apache.wicket.markup.html.form.Check;
+import org.apache.wicket.markup.html.form.CheckGroup;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.form.SimpleFormComponentLabel;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 
 import edu.unlp.informatica.postgrado.seguimiento.item.ServiceException;
 import edu.unlp.informatica.postgrado.seguimiento.item.model.ConfiguracionEstado;
@@ -13,34 +19,41 @@ import edu.unlp.informatica.postgrado.seguimiento.view.DataSourceLocator;
 public class ConfiguracionEstadoEditForm extends BaseEntityForm<ConfiguracionEstado> {
 
 	private static final long serialVersionUID = 7499289901011022854L;
-
-	private ListChoice<Estado> estado;
-
-	private ListMultipleChoice<Estado> proximosEstados;
-		
+	
+				
 	public ConfiguracionEstadoEditForm() {
 		
 		super("inputForm", new ConfiguracionEstado());		
 		
-		
-		
+		TextField<String> estadoTextField = new TextField<String>("estado");
+		estadoTextField.setLabel(new Model<String>("Nombre"));
+		estadoTextField.setEnabled(false);
+		add(estadoTextField);
+				
 		try {
 
-			add(estado = new ListChoice<Estado>("estado"));
-			estado.setChoices(DataSourceLocator.getInstance().getEstadoService().find());
-			estado.setLabel(new Model<String>("Estado"));
-			estado.setEnabled(false);
+			CheckGroup<Estado> checks = new CheckGroup<Estado>(
+					"proximosEstados");
+			
+			add(checks);
+			ListView<Estado> checksList = new ListView<Estado>(
+					"proximosEstadosCheckBox", DataSourceLocator.getInstance().getEstadoService().find()) {
+				
+				private static final long serialVersionUID = -6087603235109311980L;
+
+				@Override
+				protected void populateItem(ListItem<Estado> items) {
+					Check<Estado> check = new Check<Estado>("check", items.getModel());
+					check.setLabel(new PropertyModel<String>(items.getModel(), "nombre"));
+					items.add(check);
+					items.add(new SimpleFormComponentLabel("proximoEstado", check));
+					
+				}
+			}.setReuseItems(true);
+			checks.add(checksList);
+			
 		} catch (ServiceException e) {
 			
 		}
-
-		try {
-
-			add(proximosEstados = new ListMultipleChoice<Estado>("proximosEstados"));
-			proximosEstados.setChoices(DataSourceLocator.getInstance().getEstadoService().find());
-			proximosEstados.setLabel(new Model<String>("Próximos estados"));
-		} catch (ServiceException e) {
-			
-		}		
 	}	
 }
